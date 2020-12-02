a995594e93e7dff5bd2b05733458d29adc433760
Abigail <abigail@abigail.be>
2020-12-02 16:50:46 +0100

    An alternative solution, using just regular expressions.

---
+++ Day_02/solution-re.pl
@@ -0,0 +1,49 @@
+#!/opt/perl/bin/perl
+
+use 5.032;
+
+use strict;
+use warnings;
+no  warnings 'syntax';
+no  warnings 'experimental::regex_sets';
+
+use experimental 'signatures';
+use experimental 'lexical_subs';
+
+#
+# For the challenge description, see solution.pl.
+#
+# This is alternative solution, using just a regular expressions.
+#
+
+my $input = shift // "input";
+
+my $valid1 = 0;
+my $valid2 = 0;
+
+open my $fh, "<", $input or die "open: $!";
+while (<$fh>) {
+    $valid1 ++ if /^(?<min>    [0-9]+) \s* - \s*
+                    (?<max>    [0-9]+) \s+
+                    (?<letter> [a-z])  \s* : \s*
+                    (??{ "(?<notletters>(?[ [a-z] & [^$+{letter}]])*)"   .
+                         "(?:$+{letter}(?&notletters)){$+{min},$+{max}}" .
+                         "(?&notletters)" }) $/x;
+    $valid2 ++ if /^(?<min>    [0-9]+) \s* - \s*
+                    (?<max>    [0-9]+) \s+
+                    (?<letter> [a-z])  \s* : \s+
+                    (??{ "[a-z]{@{[$+{min}-1]}}"             .
+                         "(?:[$+{letter}]"                   . 
+                             "[a-z]{@{[$+{max}-$+{min}-1]}}" .
+                             "(?[[a-z]&[^$+{letter}]])|"     .
+                           "(?[[a-z]&[^$+{letter}]])"        .
+                             "[a-z]{@{[$+{max}-$+{min}-1]}}" .
+                             "[$+{letter}])"                 .
+                         "[a-z]*" }) $/x;
+}
+
+say "Solution 1: ", $valid1;
+say "Solution 2: ", $valid2;
+
+
+__END__
