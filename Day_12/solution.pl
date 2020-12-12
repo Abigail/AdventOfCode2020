2e1844d796af37eca9f9eed9af7f466886abc9ad
Abigail <abigail@abigail.be>
2020-12-12 18:28:22 +0100

    Some improvements

--- Day_12/solution.pl
+++ Day_12/solution.pl
@@ -12,52 +12,55 @@ use experimental 'lexical_subs';
 my $input = shift // "input";
 open my $fh, "<", $input or die "open: $!";
 
-my $NS1 =   0;        # Distance travelled NS, part 1
-my $EW1 =   0;        # Distance travelled EW, part 1
-my $NS2 =   0;        # Distance travelled NS, part 2
-my $EW2 =   0;        # Distance travelled EW, part 2
-my @HD  = ( 0,  1);   # Heading. N = (-1, 0), E = (0, 1), etc.
-my @WP  = (-1, 10);   # Waypoint.
-my $X   =   0;        # N-S index for @HD and @WP.
-my $Y   =   1;        # E-W index for @HD and @WP.
+my @P1 = ( 0,  0);   # Boat position for part 1
+my @P2 = ( 0,  0);   # Boat position for part 2
+my @HD = ( 0,  1);   # Heading. N = (-1, 0), E = (0, 1), etc.
+my @WP = (-1, 10);   # Waypoint.
+my $X  =   0;        # N-S index for @HD and @WP.
+my $Y  =   1;        # E-W index for @HD and @WP.
+
 while (<$fh>) {
     /^(?<action>[NSWELRF]) \s*
       (?<distance>[0-9]+)/x or die "Failed to parse: $_";
     my ($action, $distance) = @+ {qw [action distance]};
 
     #
-    # Change turning left by X degrees into turning right by 360 - X degrees
+    # Move the boat (part 1), or move the waypoint (part 2)
     #
+    if ($action eq 'N') {$P1 [$X] -= $distance; $WP [$X] -= $distance;}
+    if ($action eq 'E') {$P1 [$Y] += $distance; $WP [$Y] += $distance;}
+    if ($action eq 'S') {$P1 [$X] += $distance; $WP [$X] += $distance;}
+    if ($action eq 'W') {$P1 [$Y] -= $distance; $WP [$Y] -= $distance;}
+
+    #
+    # Move the boat, either in the way we're heading (part 1).
+    # or in the direction of the way point (part 2).
+    #
+    if ($action eq 'F') {$P1 [$X] += $distance * $HD [$X];
+                         $P1 [$Y] += $distance * $HD [$Y];
+                         $P2 [$X] += $distance * $WP [$X];
+                         $P2 [$Y] += $distance * $WP [$Y];}
+
     if ($action eq 'L') {
+        #
+        # Change turning left by X degrees into turning right by 360 - X degrees
+        #
         $action   = 'R';
         $distance =  360 - $distance;
     }
-
-    if    ($action eq 'N') {$NS1    -= $distance;
-                            $WP [0] -= $distance;}
-    elsif ($action eq 'E') {$EW1    += $distance;
-                            $WP [1] += $distance;}
-    elsif ($action eq 'S') {$NS1    += $distance;
-                            $WP [0] += $distance;}
-    elsif ($action eq 'W') {$EW1    -= $distance;
-                            $WP [1] -= $distance;}
-    elsif ($action eq 'F') {$NS1    += $distance * $HD [0];
-                            $EW1    += $distance * $HD [1];
-                            $NS2    += $distance * $WP [0];
-                            $EW2    += $distance * $WP [1];}
-    elsif ($action eq 'R') {
+    #
+    # Rotate the heading (part 1), or waypoint (part 2)
+    #
+    if ($action eq 'R') {
         while ($distance) {
-            @HD = ($HD [1], -$HD [0]);
-            @WP = ($WP [1], -$WP [0]);
+            @HD = ($HD [$Y], -$HD [$X]);
+            @WP = ($WP [$Y], -$WP [$X]);
             $distance -= 90;
         }
     }
-    else {
-        die "Unknown action '$action'"
-    }
 }
 
-say "Solution 1: ", abs ($NS1) + abs ($EW1);
-say "Solution 2: ", abs ($NS2) + abs ($EW2);
+say "Solution 1: ", abs ($P1 [$X]) + abs ($P1 [$Y]);
+say "Solution 2: ", abs ($P2 [$X]) + abs ($P2 [$Y]);
 
 __END__
